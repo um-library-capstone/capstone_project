64292476102e7fd16e1ec612f17cb7a5d39ee118
Kripa Srinivasan <53539831+cutebunny1@users.noreply.github.com>
2024-04-08 02:32:04 -0400

    commented and formatted code
    made much more readable and client ready

--- dash/functions.py
+++ dash/functions.py
@@ -94,40 +94,29 @@ def get_data_for_job_description_table():
     return df
 
 def get_unique_work_attribute_categories():
-
     df = get_data()
-
     return df["Work Attribute Category"].drop_duplicates().sort_values(ascending=False).tolist()
 
 
 def get_unique_decision_making_authorities():
-
     df = get_data()
-
     return df["Decision-Making Authority"].drop_duplicates().sort_values(ascending=False).tolist()
 
 
 def get_unique_functional_areas():
-
     df = get_data()
-
     return df["Functional Area"].drop_duplicates().sort_values(ascending=False).tolist()
 
 def get_unique_special_work_attributes(): 
-
     df = get_data()
-
     return df["Specific Work Attribute"].drop_duplicates().sort_values(ascending = True)
 
 def get_unique_position_titles():
-
     df = get_data()
-
     return df['Position Title'].drop_duplicates().sort_values(ascending = True)
 
 
 def get_network_data(functional_area, decision_making_authority, work_attribute_category):
-
     df = get_data()
 
     df = df.loc[
@@ -157,27 +146,26 @@ def get_network_data(functional_area, decision_making_authority, work_attribute_
     df["Specific Work Attribute"] = "➤ " + df["Specific Work Attribute"]
 
     df_data = df.copy()
-
     df = df_data.copy()
 
+    # summing up the mean values for each General Work Attribute
     df = df.groupby(["Work Attribute Category", "General Work Attribute"])["mean"].agg(["sum"]).reset_index()
-
     df.columns = ["source", "target", "weight"]
 
     df_level_1 = df.copy()
-
     df = df_data.copy()
-
+    # summing up the mean values for each Specific Work Attribute
     df = df.groupby(["General Work Attribute", "Specific Work Attribute"])["mean"].agg(["sum"]).reset_index()
-
     df.columns = ["source", "target", "weight"]
 
+    # concatenating the two dataframes
     df_level_2 = df.copy()
-
     df = pd.concat([df_level_1, df_level_2])
 
+    # removing rows with weight = 0
     df = df.loc[df["weight"] != 0]
 
+    # scaling the weight values
     df["weight"] = (df["weight"] + .5) * 5
 
     df = df.reset_index(drop=True)
@@ -194,51 +182,41 @@ def circle_packing_data(selected_skills):
     print("Selected Skills:", selected_skills)
 
     df = get_data()
-
+    # Filtering the data based on the selected skills
     df = df[(df["Specific Work Attribute"].isin(selected_skills)) & (df["Rating Value"] == 1)]
-
+    # checking for nans
     df = df.loc[df["Rating Value"].notna()]
-
+    # Adding 1 to the Rating Value to scale
     df["Rating Value"] = df["Rating Value"] + 1
 
+    # Grouping by Functional Area, Position Title, Product Area, and Specific Work Attribute
     df = df.groupby(["Functional Area", "Position Title", "Product Area", "Specific Work Attribute"])["Rating Value"].agg(["mean"]).reset_index()
-
     df = df.sort_values(df.columns.tolist())
 
+    # Adding some characters to the columns for formatting
     df["Position Title"] = df["Position Title"] + "."
-
     df["Product Area"] = df["Product Area"] + "."
-
     df["Specific Work Attribute"] = df["Specific Work Attribute"] + "-"
 
     df_data = df.copy()
-
     df = df_data.copy()
-
     df = df.groupby(["Specific Work Attribute", "Functional Area"])["mean"].agg(["sum"]).reset_index()
-
     df.columns = ["source", "target", "weight"]
 
     df_level_1 = df.copy()
-
     df = df_data.copy()
-
     df = df.groupby(["Functional Area", "Position Title"])["mean"].agg(["sum"]).reset_index()
-
     df.columns = ["source", "target", "weight"]
 
     df_level_2 = df.copy()
-
     df = pd.concat([df_level_1, df_level_2])
 
     df = df.loc[df["weight"] != 0]
-
     df["weight"] = (df["weight"] + .5) * 5
 
+    # group by source and sort by weight
     df = df.reset_index(drop=True)
-
     df = df.groupby(['source'], sort=False).apply(lambda x: x.sort_values(['weight'], ascending=False))
-
     df = df.reset_index(drop=True)
 
     return df
